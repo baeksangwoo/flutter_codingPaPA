cf6047f63450a290cad840005a197a5b339a7cb1
백상우 <qortkdn@naver.com>
2019-12-26 15:42:17 +0900

    none file add

---
+++ flutter_codingpapa/lib/main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('20억개의 앱'),
+      ),
+      body: Image.asset("assets/actionbar_camera.png"),
+    );
+  }
+
+}
