437ee4ade351f3ecb14e089b71928ed8f5f145d8
백상우 <qortkdn@naver.com>
2019-12-26 15:39:15 +0900

    코딩파파 1일차 자료

---
+++ flutter_codingpapa/android/app/src/main/kotlin/codingpapa/flutter_codingpapa/MainActivity.kt
@@ -0,0 +1,12 @@
+package codingpapa.flutter_codingpapa
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
