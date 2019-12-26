1a825b6d12a975848637528ba673b1f8a77775a3
백상우 <qortkdn@naver.com>
2019-12-26 16:22:50 +0900

    some changes push

--- flutter_codingpapa/lib/main_page.dart
+++ flutter_codingpapa/lib/main_page.dart
@@ -1,16 +1,81 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    Container(color: Colors.primaries[0],),
+    Container(color: Colors.primaries[1],),
+    Container(color: Colors.primaries[2],),
+    Container(color: Colors.primaries[3],),
+    Container(color: Colors.primaries[4],),
+    Container(color: Colors.primaries[5],),
+  ];
 
   @override
   Widget build(BuildContext context) {
+    setState(() {});
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text('20억개의 앱'),
       ),
-      body: Image.asset("assets/actionbar_camera.png"),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        unselectedItemColor: Colors.grey[500],
+        selectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color.fromRGBO(249, 249, 249, 1),
+        items: <BottomNavigationBarItem>[
+          _buildBottomNavigationBarItem(
+              activeIconPath: "assets/home_selected.png",
+              iconPath: "assets/home.png"),
+          _buildBottomNavigationBarItem(
+              activeIconPath: "assets/search_selected.png",
+              iconPath: "assets/search.png"),
+          _buildBottomNavigationBarItem(
+              activeIconPath: "assets/add.png", iconPath: "assets/add.png"),
+          _buildBottomNavigationBarItem(
+              activeIconPath: "assets/heart_selected.png",
+              iconPath: "assets/heart.png"),
+          _buildBottomNavigationBarItem(
+              activeIconPath: "assets/profile_selected.png",
+              iconPath: "assets/profile.png"),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) => _onItemTapped(index),
+      ),
     );
   }
 
+
+  void _onItemTapped(int index) {
+    setState((){
+      _selectedIndex = index;
+    });
+  }
+
 }
+
+
+BottomNavigationBarItem _buildBottomNavigationBarItem(
+    {String activeIconPath, String iconPath}) {
+  return BottomNavigationBarItem(
+    activeIcon:
+    activeIconPath == null ? null : ImageIcon(AssetImage(activeIconPath)),
+    icon: ImageIcon(AssetImage(iconPath)),
+    title: Text(''),
+  );
+}
+
