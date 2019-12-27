93ba5e660271bd174ad9e48963447ac2756854c3
백상우 <qortkdn@naver.com>
2019-12-27 15:49:25 +0900

    FlatButton() 높이가 지정되어 있는 곳에서 자동으로 세로 가운데 정렬이됨

--- flutter_codingpapa/lib/screens/profile_page.dart
+++ flutter_codingpapa/lib/screens/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codingpapa/constants/size.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -9,7 +10,8 @@ class _ProfilePageState extends State<ProfilePage> {
   bool _menuOpened = false;
   Size _size;
   double menuWidth;
-  int duration=200;
+  int duration = 200;
+
   @override
   Widget build(BuildContext context) {
     _size = MediaQuery.of(context).size;
@@ -18,31 +20,71 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Stack(
         children: <Widget>[_sideMenu(), _profile()],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _menuOpened = !_menuOpened; //화면이 바뀔 때 마다 바꾸기
-          });
-        },
-      ),
     );
   }
 
   Widget _sideMenu() {
     return AnimatedContainer(
+      width: menuWidth,
       curve: Curves.easeInOut,
-      color: Colors.redAccent,
+      color: Colors.grey[200],
       duration: Duration(milliseconds: duration),
-      transform: Matrix4.translationValues(_menuOpened ? _size.width-menuWidth : _size.width, 0, 0),
+      transform: Matrix4.translationValues(
+          _menuOpened ? _size.width - menuWidth : _size.width, 0, 0),
+      child: SafeArea(
+        child: SizedBox(
+          width: menuWidth,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              FlatButton(
+                child: Text('The beginner Coding'),
+                onPressed: null,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 
   Widget _profile() {
     return AnimatedContainer(
       curve: Curves.easeInOut,
-      color: Colors.yellowAccent,
+      color: Colors.transparent,
       duration: Duration(milliseconds: duration),
       transform: Matrix4.translationValues(_menuOpened ? -menuWidth : 0, 0, 0),
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            _usernameIconButton()
+          ],
+        ),
+      ),
     );
+  }
+
+  Row _usernameIconButton() {
+    return Row(
+            children: <Widget>[
+              Expanded(
+                  child: Padding(
+                      padding: const EdgeInsets.only(left: common_gap),
+                      child: Text(
+                        'Coding EnEnd',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ))),
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  setState(() {
+                    _menuOpened = !_menuOpened; //화면이 바뀔 때 마다 바꾸기
+                  });
+                },
+              )
+            ],
+          );
   }
 }
